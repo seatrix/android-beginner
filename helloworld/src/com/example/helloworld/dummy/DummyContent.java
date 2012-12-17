517147513f7e826b0768f831c6216eae5764541a
slieer <zxiaobi@hotmail.com>
2012-12-17 19:22:49 +0800

    comment delete.

--- helloworld/src/com/example/helloworld/dummy/DummyContent.java
+++ helloworld/src/com/example/helloworld/dummy/DummyContent.java
@@ -5,22 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
- */
 public class DummyContent {
-
-	/**
-	 * An array of sample (dummy) items.
-	 */
 	public static List<DummyItem> ITEMS = new ArrayList<DummyItem>();
-
-	/**
-	 * A map of sample (dummy) items, by ID.
-	 */
 	public static Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
 
 	static {
@@ -32,14 +18,6 @@ public class DummyContent {
 		addItem(new DummyItem("4", "Item 21"));
 		addItem(new DummyItem("5", "Item 22"));
 		addItem(new DummyItem("6", "Item 23"));
-
-		addItem(new DummyItem("7", "Item 31"));
-		addItem(new DummyItem("8", "Item 32"));
-		addItem(new DummyItem("9", "Item 33"));
-
-		addItem(new DummyItem("10", "Item 41"));
-		addItem(new DummyItem("11", "Item 42"));
-		addItem(new DummyItem("12", "Item 43"));		
 	}
 
 	private static void addItem(DummyItem item) {
@@ -47,9 +25,6 @@ public class DummyContent {
 		ITEM_MAP.put(item.id, item);
 	}
 
-	/**
-	 * A dummy item representing a piece of content.
-	 */
 	public static class DummyItem {
 		public String id;
 		public String content;
