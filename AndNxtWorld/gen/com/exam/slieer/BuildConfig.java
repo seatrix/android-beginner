4afc0d06cd6df913f940542478a4c84d66201a79
slieer <zxiaobi@hotmail.com>
2012-12-18 10:36:33 +0800

    optimized code.

---
+++ AndNxtWorld/gen/com/exam/slieer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.exam.slieer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- AndNxtWorld/src/com/exam/slieer/ItemListActivity.java
+++ AndNxtWorld/src/com/exam/slieer/ItemListActivity.java
@@ -33,30 +33,15 @@ public class ItemListActivity extends FragmentActivity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		/*单屏模式
-		 *setContentView(R.layout.activity_item_list);
-		 */
-		
 		/*左侧带导航模式
 		 */
-		setContentView(R.layout.activity_item_twopane);
-		
-		View vi = findViewById(R.id.item_detail_container);
-		if (vi != null) {
-			// The detail container view will be present only in the
-			// large-screen layouts (res/values-large and
-			// res/values-sw600dp). If this view is present, then the
-			// activity should be in two-pane mode.
-			mTwoPane = true;
-
-			// In two-pane mode, list items should be given the
-			// 'activated' state when touched.
-			FragmentManager fragmentManager = getSupportFragmentManager();
-			ItemListFragment itemListFragment = (ItemListFragment) fragmentManager.findFragmentById(R.id.item_list);
-			itemListFragment.setActivateOnItemClick(true);
-		}
-
-		// TODO: If exposing deep links into your app, handle intents here.
+		setContentView(R.layout.activity_item_twopane);		
+		mTwoPane = true;
+		// In two-pane mode, list items should be given the
+		// 'activated' state when touched.
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		ItemListFragment itemListFragment = (ItemListFragment) fragmentManager.findFragmentById(R.id.item_list);
+		itemListFragment.setActivateOnItemClick(true);
 	}
 
 	/**
@@ -80,11 +65,6 @@ public class ItemListActivity extends FragmentActivity implements
 
 		} else {
 			Log.i(TAG, "In single-pane mode");
-			// In single-pane mode, simply start the detail activity
-			// for the selected item ID.
-			//Intent detailIntent = new Intent(this, ItemDetailActivity.class);
-			//detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
-			//startActivity(detailIntent);
 		}
 	}
 }
