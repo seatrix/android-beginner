b66e6fd2dfbe36d3f9bdd72a37ec1f6572244f50
slieer <slieer@gmail.com>
2013-05-30 17:51:47 +0800

    aidl test

---
+++ AndNxtWorld/src/com/exam/slieer/services/MyService.java
@@ -0,0 +1,65 @@
+
+package com.exam.slieer.services;
+
+import com.exam.slieer.activities.AidlTestActivity;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    private static final String TAG = "AIDLService";
+    private ForActivity callback;
+
+    private void Log(String str) {
+        Log.d(TAG, "------ " + str + "------");
+    }
+
+    @Override
+    public void onCreate() {
+        Log("service create");
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Log("service start id=" + startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent t) {
+        Log("service on bind");
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log("service on destroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log("service on unbind");
+        return super.onUnbind(intent);
+    }
+
+    public void onRebind(Intent intent) {
+        Log("service on rebind");
+        super.onRebind(intent);
+    }
+
+    private final ForService.Stub mBinder = new ForService.Stub() {
+        @Override
+        public void invokCallBack() throws RemoteException {
+            callback.performAction();
+        }
+
+        @Override
+        public void registerTestCall(ForActivity cb) throws RemoteException {
+            callback = cb;
+        }
+    };
+}
