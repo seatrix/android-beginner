28251eb23be86c599fa3effa2e661f82b3a01c02
slieer@gmail.com <slieer@gmail.com>
2013-08-22 22:53:10 +0800

    ui test.

---
+++ prime-ui/src/com/slieer/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.slieer.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import com.slieer.ui.R;
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
